0751824e842fada2c052fb21e9db7f4aa5d3409a
Jonas Busseniers <j_busseniers@hotmail.com>
2017-10-18 16:43:58 +0200

    initial commit

---
+++ src/main/java/com/realdolmen/jbsbf24/scm/App.java
@@ -0,0 +1,13 @@
+package com.realdolmen.jbsbf24.scm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
